e782e08c4c7c2fa22bf4726c454f3a22774aaeec
Alexandre Callens <alekzxand@gmail.com>
2017-12-09 16:43:57 +0400

    ok

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,6 @@
 
 $(function () {
+    
 
     setTimeout(function () {
         $('.text-animate').removeClass('hidden');
@@ -38,4 +39,18 @@ $(function () {
       
     })
 })
+
+$(function () { 
+  $('[data-toggle="tooltip"]').tooltip({trigger: 'manual'}).tooltip('show');
+});  
+
+// $( window ).scroll(function() {   
+ // if($( window ).scrollTop() > 10){  // scroll down abit and get the action   
+  $(".progress-bar").each(function(){
+    each_bar_width = $(this).attr('aria-valuenow');
+    $(this).width(each_bar_width + '%');
+  });
+       
+ //  }  
+// });
 
